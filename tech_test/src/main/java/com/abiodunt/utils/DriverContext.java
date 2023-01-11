d7fb5b77be525afd46b8f1411d6858aef70feac3
Tolulope Abiodun <tabiodun@qmetric.co.uk>
2023-01-11 10:20:08 +0000

    feat: Continued configuration; created classes to setup web driver

---
+++ tech_test/src/main/java/com/abiodunt/utils/DriverContext.java
@@ -0,0 +1,47 @@
+package com.abiodunt.utils;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverContext {
+
+    private static DriverContext driverContext;
+
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+    public final static long TIMEOUT = 10;
+
+    private DriverContext() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMEOUT));
+        driver.manage().window().maximize();
+    }
+
+    public static void navigateTo(String url) {
+        driver.get(url);
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void initializeDriver() {
+        if (driverContext==null) {
+            driverContext = new DriverContext();
+        }
+    }
+
+    public static void tearDown() {
+        if (driver != null) {
+            driver.close();
+            driver.quit();
+        }
+        driverContext = null;
+    }
+}
+
