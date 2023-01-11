1be76d7230a059bb2f307b1d6a69039cab881f28
Tolulope Abiodun <tabiodun@qmetric.co.uk>
2023-01-11 15:08:48 +0000

    feat: Added explicit wait methods to PageActions abstract class

--- tech_test/src/main/java/com/abiodunt/PageActions.java
+++ tech_test/src/main/java/com/abiodunt/PageActions.java
@@ -1,5 +1,48 @@
 package com.abiodunt;
 
+import com.abiodunt.utils.DriverContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.lang.invoke.MethodHandles;
+import java.time.Duration;
+
+import static com.abiodunt.utils.DriverContext.TIMEOUT;
+
 public abstract class PageActions {
+
+    private static final Logger LOGGER = LogManager.getLogger(MethodHandles.lookup().lookupClass());
+
+    public void waitForElementPresence(String locator) {
+        try {
+            new WebDriverWait(DriverContext.getDriver(), Duration.ofSeconds(TIMEOUT))
+                    .until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator)));
+        } catch (Exception e) {
+            LOGGER.error("Element with XPath: {} not present.", locator);
+        }
+    }
+
+    public void waitForClickableElement(String locator) {
+        try {
+            new WebDriverWait(DriverContext.getDriver(), Duration.ofSeconds(TIMEOUT))
+                    .until(ExpectedConditions.elementToBeClickable(By.xpath(locator)));
+        } catch (Exception e) {
+            LOGGER.error("Element with XPath: {} not clickable.", locator);
+        }
+    }
+
+    public void waitForPageLoad() {
+        try {
+            new WebDriverWait(DriverContext.getDriver(), Duration.ofSeconds(TIMEOUT)).until(
+                    webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+        } catch (TimeoutException e) {
+            LOGGER.error("Page did not load before timeout.");
+        }
+    }
 
 }
