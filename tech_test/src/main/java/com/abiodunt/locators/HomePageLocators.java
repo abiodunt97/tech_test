9778cffa392b51128a0945666fb797ae3105de2d
Tolulope Abiodun <tabiodun@qmetric.co.uk>
2023-01-11 15:12:56 +0000

    feat: Added String and WebElement locators required for the test

--- tech_test/src/main/java/com/abiodunt/locators/HomePageLocators.java
+++ tech_test/src/main/java/com/abiodunt/locators/HomePageLocators.java
@@ -1,6 +1,41 @@
 package com.abiodunt.locators;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 public class HomePageLocators {
 
-}
+    @FindBy(css = "div[class='sc-aef7b723-0 sc-dae82938-0 coScOT']")
+    public WebElement rowSortDropdown;
+    @FindBy(xpath = "//table[@class='sc-f7a61dda-3 kCSmOD cmc-table  ']//tr//th")
+    public List<WebElement> tableHeader;
+    @FindBy(xpath = "//table[@class='sc-f7a61dda-3 kCSmOD cmc-table  ']//tr")
+    public List<WebElement> tableRows;
+    @FindBy(xpath = "//*[ text() = 'Filters' ]")
+    public WebElement filterButton;
+    @FindBy(xpath = "//input[@data-qa-id='range-filter-input-min']")
+    public WebElement minRangeInput;
+    @FindBy(xpath = "//input[@data-qa-id='range-filter-input-max']")
+    public WebElement maxRangeInput;
+    @FindBy(xpath = "//*[ contains (text(), 'Algorithm' ) ]")
+    public WebElement algorithmButton;
+    @FindBy(xpath = "//*[ text() = 'PoW' ]")
+    public WebElement powFilter;
 
+
+    public String filterButtonLocator = "//*[ text() = 'Filters' ]";
+    public String algorithmButtonLocator = "//*[ contains (text(), 'Algorithm' ) ]";
+    public String powFilterLocator = "//*[ text() = 'PoW' ]";
+    public String addFilterLocator = "//*[ text() = 'Add Filter' ]";
+    public String mineableToggleLocator = "//button[contains(text(),'Mineable')]";
+    public String allCryptocurrenciesLocator = "//button[contains(text(),'All Cryptocurrencies')]";
+    public String coinsLocator = "//button[contains(text(),'Coins')]";
+    public String priceLocator = "//button[contains(text(),'Price')]";
+    public String minRangeInputLocator = "//input[@data-qa-id='range-filter-input-max']";
+    public String maxRangeInputLocator = "//input[@data-qa-id='range-filter-input-max']";
+    public String applyFilterLocator = "//button[contains(text(),'Apply Filter')]";
+    public String showResultsLocator = "//button[contains(text(),'Show results')]";
+
+}
